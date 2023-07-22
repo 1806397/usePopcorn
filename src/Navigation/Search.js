cd055ceed7bf52ee83349f32b0b71b6b3cc742ff
Piyush Singh <1806397@kiit.ac.in>
2023-07-22 16:04:53 +0530

    Added useRef

--- src/Navigation/Search.js
+++ src/Navigation/Search.js
@@ -1,4 +1,27 @@
+import { useEffect } from "react";
+import { useRef } from "react";
+
 function Search({ query, setQuery }) {
+  const inputElement = useRef(null);
+  useEffect(
+    function () {
+      function callback(e) {
+        //checking if search bar is already on focus,
+        // if it is Enter keypress won't remove entered query in search bar
+        if (document.activeElement === inputElement.current) return;
+        //else
+        // By pressing Enter focus will be on Search bar
+        if (e.code === "Enter") {
+          inputElement.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      // Clearing the function
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
   return (
     <input
       className="search"
@@ -6,6 +29,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
